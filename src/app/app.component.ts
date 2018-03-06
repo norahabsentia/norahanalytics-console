06d7b21982cc2d5fd869e347ff0c419498c38017
Norah Absentia <vrushali@absentiavr.com>
2018-03-07 00:41:34 +0530

    Revert "Update prediction page"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,6 @@ import { AnalyticsService } from './@core/utils/analytics.service';
 
 @Component({
   selector: 'ngx-app',
-  styles: [`
-    html {
-      font-size: 14px;
-    }
-  `],
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
